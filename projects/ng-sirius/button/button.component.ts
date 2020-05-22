4b348897da0a2ec191557b78796f6514f12b4f83
unknown <zhuchenxi@ut.cn>
2020-05-22 16:16:25 +0800

    finish button

--- projects/ng-sirius/button/button.component.ts
+++ projects/ng-sirius/button/button.component.ts
@@ -1,28 +1,36 @@
-import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation, ChangeDetectionStrategy, ChangeDetectorRef} from '@angular/core';
+import {
+    Component,
+    OnInit, Input,
+    Output,
+    EventEmitter,
+    ViewEncapsulation,
+    ChangeDetectionStrategy,
+    ElementRef,
+    HostBinding
+} from '@angular/core';
 import { SirClassType } from 'ng-sirius/core/types/klass';
 
 @Component({
-	selector: 'sir-button',
-	templateUrl: './button.component.html',
-	styleUrls: ['./style/button.scss'],
-	changeDetection: ChangeDetectionStrategy.OnPush,
-	encapsulation: ViewEncapsulation.None
+    selector: `button[sir-button]`,
+    exportAs: 'sirButton',
+    templateUrl: './button.component.html',
+    styleUrls: ['./button.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    encapsulation: ViewEncapsulation.None,
 })
 export class SirButtonComponent implements OnInit {
 
-	@Input() disabled: boolean = false;
-	@Input() size: 'large' | 'default' | 'small' = 'default';
-	@Input() style: 'primary' | 'default' | 'dash' = 'primary';
-	@Input() sirClass: SirClassType = '';
+    @Input() style: 'primary' | 'default' | 'dash' = 'default';
 
-	@Output() sirClick = new EventEmitter<Event>();
+    @Input() disabled: boolean = false;
+    @HostBinding('[attr.disabled]') get isDisabled() { return this.disabled || null; }
+    @HostBinding('[class.sir-button-default]') get isDefault() { return this.style === 'default'; }
+    @HostBinding('[class.sir-button-primary]') get isPrimary() { return this.style === 'primary'; }
 
-	constructor() { }
+    constructor(private elementRef: ElementRef) {
+        (this.elementRef.nativeElement as HTMLElement).classList.add('sir-button');
+    }
 
-	ngOnInit(): void {
-	}
-
-	sirOnClick(event: Event) {
-		this.sirClick.emit(event);
-	}
+    ngOnInit(): void {
+    }
 }
