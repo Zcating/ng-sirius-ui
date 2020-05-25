0abccc87a35327d12964c5a19550995a7d6307e3
unknown <zhuchenxi@ut.cn>
2020-05-25 08:25:13 +0800

    update sku

--- projects/ng-sirius/sku/sku-graph.ts
+++ projects/ng-sirius/sku/sku-graph.ts
@@ -11,7 +11,44 @@ export class Node<T = SirAny> {
 }
 
 export class SkuGraph<T = SirAny> {
-    constructor(
+
+    static create(data: ISirSkuData) {
+        const nodes: Node<ISirSkuSpec>[] = [];
+        for (const category of data.specCategories) {
+            for (const spec of category.specs) {
+                nodes.push(new Node(spec));
+            }
+        }
+
+        for (const node of nodes) {
+            node.edges = new Array(nodes.length).fill(0);
+        }
+
+
+        for (const comb of data.combinations) {
+            for (const id1 of comb.specIds) {
+                for (const id2 of comb.specIds) {
+                    if (id1 === id2) {
+                        continue;
+                    }
+                    const x = nodes.findIndex((theNode) => {
+                        return theNode.value.id === id1;
+                    });
+                    const y = nodes.findIndex((theNode) => {
+                        return theNode.value.id === id2;
+                    });
+                    if (x === -1 || y === -1 || x === y) {
+                        continue;
+                    }
+                    nodes[x].edges[y] = 1;
+                }
+            }
+        }
+
+        return new SkuGraph(nodes);
+    }
+
+    private constructor(
         private readonly nodes: Node<T>[]
     ) { }
 
@@ -25,48 +62,21 @@ export class SkuGraph<T = SirAny> {
             return tag === theTag ? this.nodes[i].value : null;
         }) || [];
     }
+
+    getUnion(target: (T | null)[], theTag: 0 | 1, predicate: (value: T) => boolean) {
+        return union(target, this.queryNodeValuesByTag(theTag, predicate));
+    }
+
+    getIntersection(target: (T | null)[], theTag: 0 | 1, predicate: (value: T) => boolean) {
+        return intersection(target, this.queryNodeValuesByTag(theTag, predicate));
+    }
 }
 
-export function createGraph(data: ISirSkuData) {
-    const nodes: Node<ISirSkuSpec>[] = [];
-    for (const category of data.specCategories) {
-        for (const spec of category.specs) {
-            nodes.push(new Node(spec));
-        }
-    }
 
-    for (const node of nodes) {
-        node.edges = new Array(nodes.length).fill(0);
-    }
-
-
-    for (const comb of data.combinations) {
-        for (const id1 of comb.specIds) {
-            for (const id2 of comb.specIds) {
-                if (id1 === id2) {
-                    continue;
-                }
-                const x = nodes.findIndex((theNode) => {
-                    return theNode.value.id === id1;
-                });
-                const y = nodes.findIndex((theNode) => {
-                    return theNode.value.id === id2;
-                });
-                if (x === -1 || y === -1 || x === y) {
-                    continue;
-                }
-                nodes[x].edges[y] = 1;
-            }
-        }
-    }
-
-    return new SkuGraph(nodes);
-}
-
-export function union<T>(arr1: T[], arr2: T[]) {
+function union<T>(arr1: T[], arr2: T[]) {
     return arr1.map((value, index) => value === arr2[index] ? value : null);
 }
 
-export function intersection<T>(arr1: T[], arr2: T[]) {
+function intersection<T>(arr1: T[], arr2: T[]) {
     return arr1.map((value, index) => value || arr2[index] || null);
 }
