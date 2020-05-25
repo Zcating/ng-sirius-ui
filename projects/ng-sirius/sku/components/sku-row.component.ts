3a9d51506e59e28ac47869d2f340b39c9486b64d
unknown <zhuchenxi@ut.cn>
2020-05-25 08:26:16 +0800

    update sku

---
+++ projects/ng-sirius/sku/components/sku-row.component.ts
@@ -0,0 +1,52 @@
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { ISirSkuPropertyContent, ISirSkuSpec } from '../sku.model';
+
+
+
+@Component({
+    selector: 'sir-sku-row',
+    styleUrls: ['../sku.component.scss'],
+    template: `
+        <div class="sir-sku-row">
+            <header class="sir-sku-row-title">
+                {{ title }} <span class="sir-sku-row-multiple-text" *ngIf="isMultiple">（{{ multipleText }}）</span>
+            </header>
+            <section class="sir-sku-row-items">
+                <ng-container *ngFor="let item of items; let i = index">
+                    <div
+                        class="sir-sku-row-item"
+                        [class.sir-sku-row-item-selected]="item.selected"
+                        [class.sir-sku-row-item-disabled]="item.unselectable"
+                        (click)="clickItem(i, type, item)"
+                    >
+                        <img class="sir-sku-row-item-img" *ngIf="item.previewImgUrl" [src]="item.previewImgUrl">
+                        <span class="sir-sku-row-item-name">{{item.name}}</span>
+                    </div>
+                </ng-container>
+            </section>
+        </div>
+    `,
+})
+export class SirSkuRowComponent implements OnInit {
+    @Input() title: string = '';
+    @Input() isMultiple: boolean = false;
+    @Input() multipleText: string = '可多选';
+    @Input() type: 'property' | 'spec' = 'spec';
+    @Input() items?: ISirSkuPropertyContent [] | ISirSkuSpec[];
+    
+    @Output() sirOnSelect = new EventEmitter<{ type: 'property' | 'spec', item: ISirSkuPropertyContent | ISirSkuSpec}>();
+
+    selectedTags: boolean[] = [];
+
+    constructor() {
+
+    }
+
+    ngOnInit(): void {
+        
+    }
+
+    clickItem(index: number, type: 'property' | 'spec', item: ISirSkuPropertyContent | ISirSkuSpec) {
+        this.sirOnSelect.emit({ type, item});
+    }
+}
--- projects/ng-sirius/sku/sku.module.ts
+++ projects/ng-sirius/sku/sku.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { OverlayModule } from '@angular/cdk/overlay';
 
 import { SirSkuComponent } from './sku.component';
-import { SirSkuRowComponent } from './compoenents/sku-row.component';
+import { SirSkuRowComponent } from './components/sku-row.component';
 import { SirDrawerModule } from 'ng-sirius/drawer/drawer.module';
 import { SirCounterModule } from 'ng-sirius/counter';
 import { SkuService } from './sku.service';
